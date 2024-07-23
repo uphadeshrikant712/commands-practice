19bae99d684f5b223cd7f6b7e7e08ffaa112c6e5
Uphade Shrikant Bhagwat <uphadeshrikant712@gmail.com>
2024-07-23 17:34:59 +0530

    Task-2 Created

--- React-Project/src/App.jsx
+++ React-Project/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Task from './components/Day1/Task'
+import Task2 from './components/Day2/Task2'
 
 const App = () => {
   return (
     <div>
-      <Task/>
+      <Task />
+      <Task2/>
     </div>
   )
 }
---
+++ React-Project/src/components/Day2/Task2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Task2 = () => {
+  return (
+    <div>Task2</div>
+  )
+}
+
+export default Task2
